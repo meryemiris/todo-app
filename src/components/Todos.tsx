88c0893aef61a8733be06d8178527516a5496120
Meryem İris <irismeryem.96@gmail.com>
2023-08-27 13:42:02 +0300

    move TodoItems to Todos file

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,22 +1,78 @@
-import styles from "./Todos.module.css";
+import React, { useState } from "react";
 
 import Todo from "../models/todo";
 
-import TodoItem from "./TodoItem";
+import styles from "./Todos.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 interface TodosProps {
   items: Todo[];
 }
 
 const Todos: React.FC<TodosProps> = (props: TodosProps) => {
-  return (
-    <>
-      <div className={styles.container}>
-        <header>TODO LIST</header>
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
 
-        <TodoItem items={props.items} />
-      </div>
-    </>
+  const toggleCheckbox = (itemId: string) => {
+    setCheckedItems((prevChecked) =>
+      prevChecked.includes(itemId)
+        ? prevChecked.filter((id) => id !== itemId)
+        : [...prevChecked, itemId]
+    );
+  };
+
+  const deleteHandler = (itemID: string) => (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    const listItem = event.currentTarget.parentElement?.parentElement;
+    if (listItem) {
+      listItem.remove();
+    }
+
+    console.log(itemID);
+  };
+  return (
+    <div className={styles.container}>
+      <header>TODO LIST</header>
+      <ul className={styles.list}>
+        {props.items.map((item) => (
+          <li className={styles.item} key={item.id}>
+            <div>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                checked={checkedItems.includes(item.id)}
+                onChange={() => toggleCheckbox(item.id)}
+              />
+            </div>
+            <div>
+              <button
+                className={styles.deleteButton}
+                onClick={(event: React.MouseEvent) =>
+                  deleteHandler(item.id)(event)
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faTrashAlt}
+                  className={styles.deleteIcon}
+                />
+              </button>
+            </div>
+            <div>
+              <p
+                style={{
+                  textDecoration: checkedItems.includes(item.id)
+                    ? "line-through"
+                    : "none",
+                }}
+              >
+                {item.text}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
